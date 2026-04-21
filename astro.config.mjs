fa18081249f77c6b0d33fc2ec8b250082de18961
Xulio Coira <xulioc@gmail.com>
2026-04-21 13:32:04 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://energy-template.vbartalis.dev",
+  site: "https://inusuallabs.github.io",
   output: "static",
   trailingSlash: "ignore",
 });
